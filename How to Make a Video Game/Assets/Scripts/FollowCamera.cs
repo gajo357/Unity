5c01c3cb3d7bc8ed4621e586719de8fe2388152c
Gavrilo Mumovic <gajo357@gmail.com>
2017-12-01 20:25:56 +0100

    First commit

---
+++ How to Make a Video Game/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    private float _cameraFollowSpeed = 5;
+
+	// Update is called once per frame
+	private void Update ()
+    {
+        Vector3 newPosition = player.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, _cameraFollowSpeed * Time.deltaTime);
+	}
+}
---
+++ How to Make a Video Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator _playerAnimator;
+    private Rigidbody _playerRigidbody;
+
+    private float _moveHorizontal;
+    private float _moveVertical;
+    private Vector3 _movement;
+    private float _turningSpeed = 20f;
+
+    // Use this for initialization
+    private void Start ()
+    {
+        // get the Animator component from the Player GameObject
+        _playerAnimator = GetComponent<Animator>();
+        _playerRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        // get keyboard input
+        _moveHorizontal = Input.GetAxisRaw("Horizontal");
+        _moveVertical = Input.GetAxisRaw("Vertical");
+
+        // y is vertical in Unity
+        _movement = new Vector3(_moveHorizontal, 0, _moveVertical);
+    }
+
+    private void FixedUpdate()
+    {
+        // first we rotate
+        // then we move if needed be
+
+        if (_movement != Vector3.zero)
+        {
+            // create a target rotation based on the movement
+            Quaternion targetRotation = Quaternion.LookRotation(_movement, Vector3.up);
+
+            // we do not want to rotate instantly, but rather as an animation
+            // got from curent rotation, move to the new one
+            // update 20 times faster then the delta
+            Quaternion newRotation = Quaternion.Lerp(_playerRigidbody.rotation, targetRotation, _turningSpeed * Time.deltaTime);
+
+            // change the player's rotation to new incremental rotation
+            _playerRigidbody.MoveRotation(newRotation);
+
+            // actually move
+            _playerAnimator.SetFloat("Speed", 3f);
+        }
+        else
+        {
+            _playerAnimator.SetFloat("Speed", 0f);
+        }
+    }
+}
