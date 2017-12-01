314ba205c785104192a1d520db9603cf87ea1b18
Gavrilo Mumovic <gajo357@gmail.com>
2017-12-01 21:12:33 +0100

    added a fly object and a collision

---
+++ How to Make a Video Game/Assets/Scripts/FlyMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FlyMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform center;
+
+    private float _flySpeed;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        _flySpeed = Random.Range(300f, 700f);
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        transform.RotateAround(center.position, Vector3.up, _flySpeed * Time.deltaTime);
+	}
+}
---
+++ How to Make a Video Game/Assets/Scripts/FlyPickup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FlyPickup : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other)
+    {
+        // if the collider is tagged with Player
+        if(other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
