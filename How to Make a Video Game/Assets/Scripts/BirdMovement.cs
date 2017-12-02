f98f94516fe0a736ca451ab30e13a825898c666d
Gavrilo Mumovic <gajo357@gmail.com>
2017-12-02 11:27:16 +0100

    + Bird movement - basic AI + Player health (collison with the bird)

---
+++ How to Make a Video Game/Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class BirdMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private NavMeshAgent birdAgent;
+    private Animator birdAnimator;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        birdAgent = GetComponent<NavMeshAgent>();
+        birdAnimator = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    private void Update ()
+    {
+        // set the bird's destination
+        birdAgent.SetDestination(target.position);
+
+        // measure the magnitute of nav mesh velocity
+        float speed = birdAgent.velocity.magnitude;
+
+        // pass the velocity to animator component
+        birdAnimator.SetFloat("Speed", speed);
+	}
+}
---
+++ How to Make a Video Game/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public bool Alive;
+
+    [SerializeField]
+    private GameObject pickupPrefab;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        Alive = true;
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!Alive)
+            return;
+
+        // if the collider is tagged with Player
+        if (other.CompareTag("Enemy"))
+        {
+            Alive = false;
+
+            // create the pickup particles
+            Instantiate(pickupPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
